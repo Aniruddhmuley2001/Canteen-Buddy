313bd52452bdf6f9bacb21fdec6351939f1bca3a
bhavyakedar <bhavyakedar@users.noreply.github.com>
2020-11-07 17:51:52 +0530

    Database Test 1

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const functions = require('firebase-functions')
-const admin = require('firebase-admin')
-const firebase = require('firebase')
-
-var firebaseConfig = {
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const firebase = require('firebase');
+const firebaseConfig = {
     apiKey: "AIzaSyDc6od5tCOr3L8GwJ-nzWK5SfH8S9Wxo2U",
     authDomain: "food-o-click.firebaseapp.com",
     databaseURL: "https://food-o-click.firebaseio.com",
@@ -14,30 +15,56 @@ var firebaseConfig = {
     appId: "1:216202521279:web:fc692ead78d312965ff40a",
     measurementId: "G-PEHR6QTYRF"
   };
+firebase.initializeApp(firebaseConfig);
+db = admin.firestore();
+
+////////////////////////////////////////////////////
+/*
+const express = require('express')
+const bodyParser = require('body-parser')
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+const db = admin.firestore()
+admin.initializeApp();
+//const { signupParent, signupVendor } = require('./util/signup.js')
+//const { signinParent, signinVendor } = require('./util/signin.js')
+//const { FBApp } = require('./firebase-init.js')
+const firebase = require('firebase');
+
+var firebaseConfig = {
+    
+  };
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+admin.initializeApp();*/
 
-const { signupParent, signupVendor } = require('./util/signup.js')
-const { signinParent, signinVendor } = require('./util/signin.js')
-
-admin.initializeApp(functions.config().firebase)
-
-const app = express()
+//const app = express()
 const port = 3031
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-    res.send("Hi!")
-    res.end()
-})
+app.get('/parents', (req, res) => {
+    db.collection('parents').get()
+    .then((data) => {
+        let parents = [];
+        data.forEach((doc) => {
+            parents.push({
+                id : doc.id,
+                parentName : doc.data().parentName
+            });
+        });
+        console.log(parents)
+        return res.json(parents);
+     })
+     .catch(err => console.error(err));
+});
 
-app.post('/signupParent', signupParent)
-app.post('/signupVendor', signupVendor)
+//app.post('/signupParent', signupParent)
+//app.post('/signupVendor', signupVendor)
 
-app.post('/signupParent', signinParent)
-app.post('/signupVendor', signinVendor)
+//app.post('/signupParent', signinParent)
+//app.post('/signupVendor', signinVendor)
 
 app.listen(port, console.log(`listening on port ${port}`) )
 
