f94c0f13cc2a27cf88eba3a465bde7a573c40dab
bhavyakedar <bhavyakedar@users.noreply.github.com>
2020-11-07 15:21:22 +0530

    Dependencies Changed.

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -2,6 +2,20 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
+const firebase = require('firebase')
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDc6od5tCOr3L8GwJ-nzWK5SfH8S9Wxo2U",
+    authDomain: "food-o-click.firebaseapp.com",
+    databaseURL: "https://food-o-click.firebaseio.com",
+    projectId: "food-o-click",
+    storageBucket: "food-o-click.appspot.com",
+    messagingSenderId: "216202521279",
+    appId: "1:216202521279:web:fc692ead78d312965ff40a",
+    measurementId: "G-PEHR6QTYRF"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 const { signupParent, signupVendor } = require('./util/signup.js')
 const { signinParent, signinVendor } = require('./util/signin.js')
