c9ef59670c6c863c4af1d573de9503d7001c0c3b
Travis CI <travis>
2020-11-07 14:34:36 +0530

    setup server

---
+++ firebase-functions/functions/util/signup.js
@@ -0,0 +1,12 @@
+const functions = require('firebase-functions')
+
+function signupParent(req, res) {
+    res.end()
+}
+
+function signupVendor(req, res) {
+    res.end()
+}
+
+exports.signupParent = signupParent
+exports.signupVendor = signupVendor
